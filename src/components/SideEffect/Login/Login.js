1fb62037a4dcbebb7924a492f2a3a93ccd87dfb1
leehah <leehah0908@naver.com>
2024-09-13 17:28:18 +0900

    24.09.13

--- src/components/SideEffect/Login/Login.js
+++ src/components/SideEffect/Login/Login.js
@@ -1,43 +1,95 @@
-import React, { useState } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import Card from '../../UI/Card';
 import styles from './Login.module.css';
 import ButtonComponent from '../../UI/Button';
 
-const Login = () => {
-  // 이메일 입력값을 저장
-  const [enteredEmail, setEnteredEmail] = useState('');
+// 리듀서 함수 선언
+/*
+   이 컴포넌트에서 사용하는 모든 상태와 상태 변경을 중앙 제어하는 함수.
+   컴포넌트 내부 데이터를 사용하지 않고 상태에만 집중하기 때문에
+   컴포넌트 바깥쪽에 선언하는 것이 일반적입니다.
+   
+   param1 - state: 변경 전의 상태값
+   param2 - action: dispatch함수(상태 변경 등의 행동)가 전달한 상태 변경 객체
+   return: 관리할 상태값들을 반환
+ */
+const emailReducer = (state, action) => {
+  if (action.type === 'USER_INPUT') {
+    return {
+      value: action.val,
+      isValid: action.val.includes('@'),
+    };
+  } else if (action.type === 'INPUT_VALIDATE') {
+    return {
+      value: state.value,
+      isValid: state.value.includes('@'),
+    };
+  }
+};
+
+const Login = ({ onLogin }) => {
+  // email reducer 사용하기
+  /*
+     param1 - reducer function: 위에서 만든 리듀서 함수
+     param2 - initial state: 초기 상태값
+     return1 - 이메일 관련 상태변수
+     return2 - dispatch함수: 상태를 변경할 수 있는 함수
+   */
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: '',
+    isValid: null,
+  });
 
   // 패스워드 입력값을 저장
   const [enteredPw, setEnteredPw] = useState('');
 
-  // 이메일 입력이 정상인지 확인
-  const [emailIsValid, setEmailIsValid] = useState();
-
   // 패스워드 입력이 정상인지 확인
-  const [pwIsValid, setPwIsValid] = useState();
+  const [pwIsValid, setPwIsValid] = useState(true);
 
   // 이메일, 패스워드가 둘 다 동시에 정상적인 상태인지 확인
   const [formIsValid, setFormIsValid] = useState(false);
 
+  // 기존의 email 상태변수를 제거함
+  // 상태값이 필요하다면 -> reducer가 제공한 상태값을 활용
+  // 내가 지정한 상태값에서 필요한 값들을 뽑아서 사용.
+  const { value: emailValue, isValid: emailIsValid } = emailState;
+
+  useEffect(() => {
+    // setTimeout을 사용해서 유혀성 검증을 1초 뒤에 실행하도록 작성
+    // 1초 이내에 새로운 입력값이 들어온다면 -> 상태변경 -> useEffect가 다시 실행됨
+    const timer = setTimeout(() => {
+      setFormIsValid(emailIsValid && enteredPw.trim().length > 6);
+    }, 1000);
+
+    // cleanup 함수: 컴포넌트가 업데이트 되거나, 없어지기 직전에 실행
+    // 사용자가 1초 이내에 추가 입력 -> 상태변경 (컴포넌트 업데이트) -> 위에 예약한 timer를 취소
+    return () => {
+      clearTimeout(timer);
+    };
+    // 의존성 배열에 상태변수를 넣어주면 그 상태변수가 변경될 때마다 useEffet가 재실행됨
+  }, [emailValue, enteredPw]);
+
   // Handler 선언
   // 이메일이 변경될 때마다 실행할 핸들러
   const emailChangeHandler = (e) => {
-    setEnteredEmail(e.target.value);
-
-    setFormIsValid(e.target.value.includes('@') && enteredPw.trim().length > 6);
+    // reducer의 상태 변경은 dispatch 함수를 통해서 처리
+    // dispatch함수의 매개값 객체의 key는 정해진 것이 아닌, reducer 함수에서 구분하기 위해 붙여주는 이름.
+    // 프로퍼티의 key와 value는 자유롭게 줄 수 있습니다. (정해진 게 아님!)
+    dispatchEmail({
+      type: 'USER_INPUT',
+      val: e.target.value,
+    });
   };
 
   // 비밀번호가 변경될 때마다 실행할 핸들러
   const passwordChangeHandler = (e) => {
     setEnteredPw(e.target.value);
-
-    setFormIsValid(
-      e.target.value.trim().length > 6 && enteredEmail.includes('@'),
-    );
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    dispatchEmail({
+      type: 'INPUT_VALIDATE',
+    });
   };
 
   const validatePasswordHandler = () => {
@@ -46,7 +98,7 @@ const Login = () => {
 
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log('더ㅗㅇ작');
+    onLogin(emailValue, enteredPw);
   };
 
   return (
@@ -59,7 +111,7 @@ const Login = () => {
           <input
             type='email'
             id='email'
-            value={enteredEmail}
+            value={emailValue}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler} // blur는 focus의 반대
           />
